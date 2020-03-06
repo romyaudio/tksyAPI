1e9eb3ce146207d909f9f709f8b1cc6616529d7d
romyaudio <romyaudio@hotmail.com>
2020-03-06 13:20:39 -0500

    building

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\User as UserResource;
+use App\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/user', function () {
+    return new UserResource(User::find(31));
 });
